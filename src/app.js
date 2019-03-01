8b277e117d55d71950f606421c2f971bd6051fb0
T-Wesst <code.daddy17@gmail.com>
2019-02-28 22:42:49 -0800

    async await function regenerator runtime error

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 import { person, sayHello } from './lib';
 console.timeLog(person.name);
 console.log(sayHello('Tyroo'));
+
+// async label auto returns promise
+async function getPosts() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  return data;
+}
+
+getPosts().then(posts => console.log(posts));
