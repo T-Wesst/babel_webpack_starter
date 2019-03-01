26b18925671f916b257c4fbcb3ceda97073f9967
T-Wesst <code.daddy17@gmail.com>
2019-02-28 21:25:14 -0800

    define config file: entry, output and module

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,26 @@
 // defines source files and output
 const path = require('path');
+module.exports = {
+  // Entry point for es6 and es7 and beyond files
+  entry: {
+    app: './src/app.js'
+  },
+  // compiled es6 and es7 and beyond javascript into es5
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js'
+  },
+  // babel module as loader
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        }
+      }
+    ]
+  }
+};
